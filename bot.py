d63802f10b0029237eb1885938ca30c8488de977
Piyush Bhatt <gavinharris123@gmail.com>
2026-02-25 21:58:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import discord
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
+from config import load_config, save_config
 
 # ---------------- ENV VARIABLES ----------------
 load_dotenv()
@@ -14,4 +15,21 @@ intents.messages = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+# ---------------- BOT SETUP COMMAND FOR SERVER ----------------
+@bot.command(name="setupArchiveBot")
+@commands.has_permissions(administrator=True)
+async def setup(ctx, forum_channel_id: int, archive_channel_id: int):
+    """
+    Admin command: !setupArchiveBot <forum_channel_id> <archive_channel_id>
+    """
+    config = load_config()
+    config[str(ctx.guild.id)] = {
+        "forum": forum_channel_id,
+        "archive": archive_channel_id
+    }
+    save_config(config)
+    await ctx.send(f"ArchiveBot setup complete!\nForum channel ID: {forum_channel_id}\nArchive channel ID: {archive_channel_id}")
+
+# ---------------- RUN BOT ----------------
 bot.run(TOKEN)
